a90020b1f1154df44751ef201aae2808747fb5a2
Franklin Hau <fhauchi@gmail.com>
2023-03-08 04:13:56 -0700

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(name3, value){
+    const namesmessage = [];
+    for (let i = 0; i < name3.length; i++){
+        namesmessage.push(`Thank you, ${name3[i]}, for the wonderful ${value} gift!`);
+    }
+    
+    return namesmessage
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+let number10 = 10
+function countDown(number10){
+    let i = 0
+    while (number10 >= i) {
+    console.log(number10--);
+}
+    return number10
+}
+
